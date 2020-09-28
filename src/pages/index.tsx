ce8f97f2d9e6cae87a4e622b28eb7af06158cb81
Elias Moreno <eliasmoreno@Elias.local>
2020-09-28 13:52:04 -0500

    next app + chakra + typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Heading, Flex } from "@chakra-ui/core";
+
+const Index = () => {
+  return (
+    <Flex>
+      <Heading>Welcome to My web site!</Heading>
+    </Flex>
+  );
+};
+
+export default Index;
